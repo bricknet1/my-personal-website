6bccbea84cf31af29d7a0c11f621ca810b50c244
Nick Johnson <nlj222@gmail.com>
2022-12-28 15:08:40 -0800

    added text content and main photo

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+/*
+
+// this was setup code from the lesson
+
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+
+*/
+
+const bigFace = document.getElementById("avatar");
+
+function moveFace() {
+    const randX = Math.floor(Math.random() * (window.innerWidth - 100));
+    const randY = Math.floor(Math.random() * (window.innerHeight - 100));
+    console.log([randX, randY]);
+    bigFace.animate({"left": randX + "px", "top": randY + "px"});
+}
+
+function gotMe() {
+    alert("Got me!")
+}
+
+bigFace.addEventListener("mouseover", moveFace);
+bigFace.addEventListener("click", gotMe)
+
